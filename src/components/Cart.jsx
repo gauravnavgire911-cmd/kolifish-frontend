0ba6ef3d947ac59f7ea9ccf7d29811976f9446e3
gauravnavgire911-cmd <gauravnavgire911@gmail.com>
2026-03-03 04:01:06 +0530

    Fix CartContext import path in Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CartContext } from "../context/CartContext.jsx";
+import { CartContext } from "../Context/CartContext.jsx";
 
 const Cart = ({ isOpen, onClose }) => {
   const { cart, cartTotal, removeFromCart, updateQuantity, clearCart } = useCart();
